aab10081a21ba5cdfe290164f96cc62a10ca6f4e
unknown <>
2017-05-03 19:25:48 +0800

    Button1

--- 404229/Form1.cs
+++ 404229/Form1.cs
@@ -21,5 +21,10 @@ namespace _404229
         {
             MessageBox.Show("404229");
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("KILLER");
+        }
     }
 }
